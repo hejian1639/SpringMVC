88b4872d9d0901f4f3e66624db9dd10de7feb66b
何健 <john.he@fairlinkcentury.com>
2017-07-02 13:24:24 +0800

    add spring bean util

--- src/org/spring/servlet/ContextStartListener.java
+++ src/org/spring/servlet/ContextStartListener.java
@@ -3,6 +3,9 @@ package org.spring.servlet;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
+import org.spring.service.SpringBeanUtils;
+import org.spring.service.HelloWorldService;
+
 /**
  * 程序启动部署时,启动事务引擎
  *
@@ -16,6 +19,9 @@ public class ContextStartListener implements ServletContextListener {
 
 	@Override
 	public void contextInitialized(ServletContextEvent event) {
+		HelloWorldService service = (HelloWorldService) SpringBeanUtils.getBeanByName("helloWorldService");
+        System.out.println(service.getHelloMessage()); 
+		
         String param = event.getServletContext().getInitParameter("contextConfigLocation");
         System.out.println("param: "+ param); 
 	    System.out.println("web.root:"+ System.getProperty("springMVC.root")); 
